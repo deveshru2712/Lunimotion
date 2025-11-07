d3935d6723872d374193221d1f39f4280074dbed
Devesh Chandra <deveshru2712@gmail.com>
2025-11-07 11:39:10 +0530

    update: next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import { NextConfig } from "next";
+
+const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  compiler: {
+    removeConsole: isProd,
+  },
 };
 
 export default nextConfig;
